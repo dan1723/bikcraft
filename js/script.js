29e65691a6b516be041f0a64c353f6eb775d8185
Danilo Fernandes <dan.gomes1723@gmail.com>
2025-10-06 15:17:45 -0300

    Galeria de Bicicletas

--- js/script.js
+++ js/script.js
@@ -2,48 +2,63 @@
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
-    const url = location.href;
-    const href = link.href;
+  const url = location.href;
+  const href = link.href;
 
-    if (url.includes(href)) {
-        link.classList.add("ativo")
-    }
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
 }
 
 links.forEach(ativarLink);
-
 
 // Ativar Items do Orçamento
 const parametros = new URLSearchParams(location.search);
 
 function ativarProduto(parametro) {
-    const elemento = document.getElementById(parametro);
-    if (elemento) {
-        elemento.checked = true
-    }
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
 }
 
 parametros.forEach(ativarProduto);
 
-
-
 // Perguntas Frequentes
- const perguntas = document.querySelectorAll(".perguntas button");
+const perguntas = document.querySelectorAll(".perguntas button");
 
- function ativarPergunta(event) {
-    const pergunta = event.currentTarget;
-    const controls = pergunta.getAttribute('aria-controls');
-    const resposta = document.getElementById(controls);
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
 
-    resposta.classList.toggle("ativa");
-    const ativa = resposta.classList.contains("ativa");
-    console.log(ativa);
-    pergunta.setAttribute('aria-expanded', ativa);
- }
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  console.log(ativa);
+  pergunta.setAttribute("aria-expanded", ativa);
+}
 
- function eventosPerguntas(pergunta) {
-    pergunta.addEventListener('click', ativarPergunta);
- }
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
 
- perguntas.forEach(eventosPerguntas);
+perguntas.forEach(eventosPerguntas);
 
+// Galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  console.log(media);
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
